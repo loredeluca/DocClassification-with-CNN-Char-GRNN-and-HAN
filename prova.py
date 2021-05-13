c35295e2790c0ef098e0797620cdd05ff5a92dfa
loredeluca <loredeluca@hotmail.it>
2021-05-13 13:04:06 +0200

    Delete prova.py

--- prova.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def readIMDB():
-    dataset_name = 'imdb_reviews'
-    data_df = pd.read_csv('datasets/'+dataset_name+'.csv')
-    print("shape df: ", data_df.shape)
-
-    return dataset_name, 2, data_df
